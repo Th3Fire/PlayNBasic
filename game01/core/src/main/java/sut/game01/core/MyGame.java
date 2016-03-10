101ed3932edc370ba56e2877adb74a5f6ed7808b
b5601233 <tazakunk@gmail.com>
2016-03-10 11:11:14 +0700

    add 2 object and set speed

--- game01/core/src/main/java/sut/game01/core/MyGame.java
+++ game01/core/src/main/java/sut/game01/core/MyGame.java
@@ -16,12 +16,16 @@ public class MyGame extends Game.Default {
   private Image sunImage;
   private ImageLayer sun;
 
+  private Image fImage;
+  private ImageLayer f;
+
   private float x = 0f;
   private float y = 480f;
 
   private float a = 0f;
-  private float b = 100f;
-  private boolean flag = false;
+  private float b = 50f;
+  private boolean check1 = false;
+  private boolean check2 = false;
 
   public MyGame() {
     super(33); // call update every 33ms (30 times per second)
@@ -41,6 +45,11 @@ public class MyGame extends Game.Default {
     graphics().rootLayer().add(sun);
     sun.setTranslation(x,y);
 
+    fImage = assets().getImage("images/t.png");
+    f = graphics().createImageLayer(fImage);
+    graphics().rootLayer().add(f);
+    f.setTranslation(a,b);
+
     mountImage = assets().getImage("images/m.png");
     mount = graphics().createImageLayer(mountImage);
     graphics().rootLayer().add(mount);
@@ -54,23 +63,41 @@ public class MyGame extends Game.Default {
   public void paint(float alpha) {
     // the background automatically paints itself, so no need to do anything here!
 
-    System.out.printf("alpha: %f x : %.2f   y :%.2f bool:%B\n",alpha,x,y,flag);
-    if(!(flag)){ // ขึ้น
+    System.out.printf("alpha: %f x : %.2f   y :%.2f bool:%B a:%.2f b:%.2f\n",alpha,x,y,check1,a,b);
+    //////////////////////////////
+    if(!(check1)){ // ขึ้น
       y -= 2f * alpha;
       x += 2f * alpha;
+
     }
+
     else{  //ลง
       y += 2f * alpha;
       x -= 2f * alpha;
     }
-    if(y > 480 && flag == true){
-      flag = false;
+    /////////////////////////////////
+    if(!(check2)){
+      a += 5f * alpha;
+
+    }else{
+      a -= 5f * alpha;
+    }
+    ////////////////////////////////
+
+    if(y > 480 && check1 == true){
+      check1 = false;
 
     }
-    else if(y < 0 && flag == false){
-      flag = true;
+    else if(y < 0 && check1 == false){
+      check1 = true;
+    }
+    if(a > 520 && check2 == false){
+      check2 = true;
+    }else if(a < - 30 && check2 == true){
+      check2 = false;
     }
     sun.setTranslation(x,y);
+    f.setTranslation(a,b);
 
 
 
